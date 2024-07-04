f9f4ae7e3787fce54e48ad731095f57058a71d56
emzilia <jack.star509@gmail.com>
2024-07-04 08:03:46 -0400

    added more 'error' checking to git_decrypt, changed tabs to spaces

--- gitcrypty.sh
+++ gitcrypty.sh
@@ -1,82 +1,92 @@
 #!/bin/sh
+#
+# Encrypts files before adding them to a git repo, 
 
 cypher="aes-256-cbc"
 totalargs="$#"
-inputfile="$2"
+first_arg="$1"
+second_arg="$2"
 
 # prints help readout
 print_help() {
-	printf "Usage: gitcrypty [add/decrypt] (file)\n"
-	printf "\tEncrypt/decrypt files before pushing to a git repository.\n\n"
-	printf "\tadd\tEncrypts the file, then add its to the repo\n"
-	printf "\tdecrypt\tDecrypts all encrypted files within the folder\n"
+  printf "Usage: gitcrypty [add/decrypt] (file)\n"
+  printf "\tEncrypt/decrypt files before pushing to a git repository.\n\n"
+  printf "\tadd\tEncrypts the file, then add its to the repo\n"
+  printf "\tdecrypt\tDecrypts all encrypted files within the folder\n"
 }
 
 # tries to decrypt all files in the directory if the 2nd arg is 'decrypt'
 git_decrypt() {
-	for file in *; do
-		if [ -f "$file" ]; then
-			# head works by itself for plain ACII files but the grep pipe is necessary
-			# for files that aren't, such as docx etc. grep errors get redirected to 
-			# /dev/null (not great, but necessary to avoid redundant grep warnings about
-			# parsing binary files
-			if [ "$(head -c 6 "$file" | grep -v ''\x00'' 2>/dev/null)" = "Salted" ]; then
-				printf "Decrypting %s...\n" "$file"
-				eval "openssl "$cypher" -d -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file.d""
-				if [ "$?" ]; then
-					printf "File decryption successful\n"
-					mv "$file.d" "$file"
-					enc="$file"
-				else
-					printf "Error: decryption of %s was unsuccessful, file unchanged\n" "$file"
-				fi
-			fi
-                       if ! [ "$enc" = $file ]; then
-			       printf "Skipping unencrypted file %s...\n" "$file"
-		       fi   
-		fi
-	done
-	exit 0
+  for file in *; do
+    if [ -f "$file" ]; then
+      # head works by itself for plain ACII files but the grep pipe is necessary
+      # for files that aren't, such as docx etc. grep errors get redirected to 
+      # /dev/null (not great, but necessary to avoid redundant grep warnings about
+      # parsing binary files with null bytes
+      if [ "$(head -c 6 "$file" | grep -v ''\x00'' 2>/dev/null)" = "Salted" ]; then
+        printf "Decrypting %s...\n" "$file"
+        eval "openssl "$cypher" -d -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file.d""
+        if [ "$?" ]; then
+          printf "File decryption successful\n"
+          mv "$file.d" "$file"
+          enc="$file"
+        else
+          printf "Error: decryption of %s was unsuccessful, file unchanged\n" "$file"
+        fi
+      fi
+        if ! [ "$enc" = $file ]; then
+          printf "Skipping unencrypted file %s...\n" "$file"
+        fi   
+    fi
+  done
+  exit 0
 }
 
 git_add() {
-	if [ "$totalargs" -eq 2 ]; then
-		if [ -f "$inputfile" ]; then
-			printf "Encrypting %s...\n" "$inputfile"
-			eval "openssl "$cypher" -e -pbkdf2 -pass pass:"$GITCRYPTY" -in "$inputfile" -out "$inputfile.e""
-			if [ "$?" ]; then
-				printf "File encryption successful\n"
-				mv "$inputfile.e" "$inputfile"
-				git add "$inputfile"
-				exit 0
-			else
-				printf "Error: file encryption unsuccessful, file unchanged\n"
-				exit 1
-			fi
-		else
-			printf "Error: %s wasn't found in the working directory\n" "$inputfile"
-			exit 1
-		fi
-	else
-		printf "Error: no file specified\n"
-		exit 1
-	fi
-
+  if [ "$totalargs" -eq 2 ]; then
+    # ensures the file is actually writeable by the user before encrypting
+    if [ -w "$second_arg" ]; then
+      printf "Encrypting %s...\n" "$second_arg"
+      eval "openssl "$cypher" -e -pbkdf2 -pass pass:"$GITCRYPTY" -in "$second_arg" -out "$second_arg.e""
+      if [ "$?" ]; then
+        printf "File encryption successful\n"
+        mv "$second_arg.e" "$second_arg"
+        if ! [ "$?" ]; then
+          printf "Error: unable to overwrite file, file unchanged\n"
+          exit 1
+        fi
+        git add "$second_arg"
+        if [ "$?" ]; then
+          printf "File added to git repo\n"
+        fi
+        exit 0
+      else
+        printf "Error: file encryption unsuccessful, file unchanged\n"
+        exit 1
+      fi
+    else
+      printf "Error: %s wasn't found in the working directory\n" "$second_arg"
+      exit 1
+    fi
+  else
+    printf "Error: no file specified\n"
+    exit 1
+  fi
 }
 
 # Only runs if within a git repo
 # a more elegant method would allow use in other folders within the repo, maybe later
 if ! [ -d ".git" ]; then
-	printf "Error: No git repository found, must be run from the root\n"
-	exit 0
+  printf "Error: No git repository found, must be run from the root directory\n"
+  exit 0
 fi
 
 case $1 in
-	"decrypt")
-		git_decrypt ;;
-	"add")
-		git_add ;;
-	*)
-		print_help ;;
+  "decrypt")
+    git_decrypt ;;
+  "add")
+    git_add ;;
+  *)
+    print_help ;;
 esac
 
