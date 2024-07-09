b5fed650f113307fc824afc004164dab02eb1064
emzilia <jack.star509@gmail.com>
2024-07-09 01:23:05 -0400

    added git_pull func

--- gitcrypty.sh
+++ gitcrypty.sh
@@ -3,16 +3,14 @@
 # Encrypts files before adding them to a git repo, 
 
 cypher="aes-256-cbc"
-total_args="$#"
 first_arg="$1"
-second_arg="$2"
 
 # prints help readout
 print_help() {
   printf "Usage: gitcrypty [add/decrypt] (file)\n"
   printf "\tEncrypt/decrypt files before pushing to a git repository.\n\n"
-  printf "\tadd\tEncrypts the file, then add its to the repo\n"
-  printf "\tdecrypt\tDecrypts all encrypted files within the folder\n"
+  printf "\tadd\tEncrypts all files in the dir, then add its to the repo\n"
+  printf "\tpull\tPulls changes, then decrypts all encrypted files within the repo\n"
 }
 
 # tries to decrypt all files in the directory if the 2nd arg is 'decrypt'
@@ -25,37 +23,44 @@ git_decrypt() {
       # parsing binary files with null bytes
       if [ "$(head -c 6 "$file" | grep -v ''\x00'' 2>/dev/null)" = "Salted" ]; then
         printf "Decrypting %s...\n" "$file"
-        eval "openssl "$cypher" -d -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file.d""
+        openssl "$cypher" -d -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file".d
         if [ "$?" ]; then
           printf "File decryption successful\n"
-          mv "$file.d" "$file"
+          mv "$file".d "$file"
           enc="$file"
         else
           printf "Error: decryption of %s was unsuccessful, file unchanged\n" "$file"
         fi
       fi
-        if ! [ "$enc" = $file ]; then
-          printf "Skipping unencrypted file %s...\n" "$file"
-        fi   
+      if ! [ "$enc" = "$file" ]; then
+        printf "Skipping unencrypted file %s...\n" "$file"
+      fi   
     fi
   done
   exit 0
 }
 
+git_pull() {
+ git fetch 
+ if ! [ $(git pull --rebase) = "Already up to date."]; then
+   git_decrypt
+ fi
+}
+
 git_add() {
-  if [ "$total_args" -eq 2 ]; then
+  for file in *; do
     # ensures the file is actually writeable by the user before encrypting
-    if [ -w "$second_arg" ]; then
-      printf "Encrypting %s...\n" "$second_arg"
-      eval "openssl "$cypher" -e -pbkdf2 -pass pass:"$GITCRYPTY" -in "$second_arg" -out "$second_arg.e""
+    if [ -w "$file" ]; then
+      printf "Encrypting %s...\n" "$file"
+      openssl "$cypher" -e -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file".e
       if [ "$?" ]; then
         printf "File encryption successful\n"
-        mv "$second_arg.e" "$second_arg"
+        mv "$file".e "$file"
         if ! [ "$?" ]; then
           printf "Error: unable to overwrite file, file unchanged\n"
           exit 1
         fi
-        git add "$second_arg"
+        git add "$file"
         if [ "$?" ]; then
           printf "File added to git repo\n"
         fi
@@ -65,13 +70,11 @@ git_add() {
         exit 1
       fi
     else
-      printf "Error: %s wasn't found in the working directory\n" "$second_arg"
+      printf "Error: %s not a writeable file\n" "$file"
       exit 1
     fi
-  else
-    printf "Error: no file specified\n"
-    exit 1
-  fi
+  done
+  exit 0
 }
 
 main() {
@@ -83,8 +86,8 @@ main() {
   fi
 
   case $first_arg in
-    "decrypt")
-      git_decrypt ;;
+    "pull")
+      git_pull ;;
     "add")
       git_add ;;
     *)
