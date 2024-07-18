4f0cde4e9cde71a9af3b1f2b1f1947a7573a02cc
emzilia <jack.star509@gmail.com>
2024-07-18 14:09:40 -0400

    cleaned up printfs, added last case to git_decrypt()

--- gitcrypty.sh
+++ gitcrypty.sh
@@ -16,25 +16,27 @@ print_help() {
 # tries to decrypt all files in the directory if the 2nd arg is 'decrypt'
 git_decrypt() {
   for file in *.e; do
+    original_name="${file%.e}"
     if [ -f "$file" ]; then
       # head works by itself for plain ACII files but the grep pipe is necessary
       # for files that aren't, such as docx etc. grep errors get redirected to 
       # /dev/null (not great, but necessary to avoid redundant grep warnings about
       # parsing binary files with null bytes
-      if [ "$(head -c 6 "$file" | grep -v ''\x00'' 2>/dev/null)" = "Salted" ]; then
+      if [ "$(head -c 6 "$file" | grep -v '\x00' 2>/dev/null)" = "Salted" ]; then
         printf "Decrypting %s...\n" "$file"
         openssl "$cypher" -d -none -pbkdf2 -pass pass:"$GITCRYPTY" -in "$file" -out "$file".d
         if [ "$?" ]; then
           printf "File decryption successful\n"
-          mv "$file".d "$file"
-          enc="$file"
+          mv "$file".d "$original_name"
+          rm "$file"
         else
           printf "Error: decryption of %s was unsuccessful, file unchanged\n" "$file"
         fi
       fi
-      if ! [ "$enc" = "$file" ]; then
-        printf "Skipping unencrypted file %s...\n" "$file"
-      fi   
+      case "$original_name" in
+        *.tar)
+          git_untar "$original_name" ;;
+      esac
     fi
   done
   exit 0
@@ -42,22 +44,36 @@ git_decrypt() {
 
 git_pull() {
  git fetch 
- if ! [ $(git pull --rebase) = "Already up to date."]; then
+ if ! [ "$(git pull --rebase)" = "Already up to date." ]; then
    git_decrypt
  fi
 }
 
 git_tar() {
-  tar_file="$1"
-  printf "Found dir, archiving now"
-  if [ -w "$tar_file" ]; then
-    tar -cf "$tar_file".tar "$tar_file"
+  tar_dir="$1"
+  printf "Found dir %s, archiving now\n" "$tar_dir"
+  if [ -w "$tar_dir" ]; then
+    tar -cf "$tar_dir".tar "$tar_dir"
     if [ "$?" ]; then
-      printf "Archiving %s was successful\n" $1
+      printf "Archiving %s was successful\n" "$tar_dir"
+      # !!!!!
+      rm -r "$tar_dir"
     else
-      printf "Archiving %s failed\n" $1
+      printf "Archiving %s failed\n" "$tar_dir"
       exit 1
     fi
+  fi
+}
+
+git_untar() {
+  untar_file="$1"
+  printf "Found archive %s, extracting now\n" "$untar_file"
+  tar -xf "$untar_file"
+  if [ "$?" ]; then
+    rm "$untar_file"
+    printf "Archive %s extraction successful\n" "$untar_file"
+  else
+    printf "Archive %s extraction unsuccessful\n" "$untar_file"
   fi
 }
 
@@ -76,6 +92,10 @@ git_encrypt() {
 
 git_add() {
   for file in *; do
+    case "$file" in
+      *.e)
+        continue ;;
+    esac
     # if it's a directory, tar it before encryption 
     if [ -d "$file" ]; then
       git_tar "$file"
@@ -86,26 +106,28 @@ git_add() {
     if [ -f "$file".e ]; then
       git add --dry-run "$file".e
       if [ "$?" ]; then
-       git add "$file".e
-       if [ "$?" ]; then
-         printf "File %s was encrypted and added to the repo" "$file"
-       else
-         printf "File %s wasn't added to the repo" "$file"
-       fi
+        git add "$file".e
+      if [ "$?" ]; then
+        printf "File %s was encrypted and added to the repo\n" "$file"
+        rm "$file"
       else
-       printf "File %s wasn't added to the repo" "$file"
+        printf "File %s wasn't added to the repo\n" "$file"
+      fi
+      else
+        printf "File %s wasn't added to the repo\n" "$file"
       fi
     elif [ -f "$file".tar.e ]; then
       git add --dry-run "$file".tar.e
       if [ "$?" ]; then
-       git add "$file".tar.e
-       if [ "$?" ]; then
-         printf "File %s was encrypted and added to the repo" "$file"
-       else
-         printf "File %s wasn't added to the repo" "$file"
-       fi
+        git add "$file".tar.e
+        if [ "$?" ]; then
+          printf "File %s was encrypted and added to the repo\n" "$file"
+          rm "$file".tar
+        else
+        printf "File %s wasn't added to the repo\n" "$file"
+        fi
       else
-       printf "File %s wasn't added to the repo" "$file"
+        printf "File %s wasn't added to the repo\n" "$file"
       fi
     fi
   done
