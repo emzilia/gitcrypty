de80673b6f11b83498d50a992d1d797f258d1060
emzilia <em@zilia.dev>
2025-03-20 17:26:27 -0400

    revised comment for clarity

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-SCRIPT='gitcrypty.sh'
-SCRIPTPATH="${HOME}/.local/bin/gitcrypty"
+script='gitcrypty.sh'
+scriptpath="${HOME}/.local/bin/gitcrypty"
 
-# If file exists, remove it, if it doesn't, copy it over.
-if [ -f "${SCRIPTPATH}" ]; then
+# If the script has been installed, remove it, otherwise copy it over
+if [ -f "${scriptpath}" ]; then
 	printf "Uninstalling gitcrypty script\n"
-	rm "${SCRIPTPATH}"
-	if [ ! -f "${SCRIPTPATH}" ]; then
+	rm "${scriptpath}"
+	if [ ! -f "${scriptpath}" ]; then
 		printf "Script successfully uninstalled!\n"
 	else
 		printf "Error: Uninstallation failed\n"
 	fi
-elif [ ! -f "${SCRIPTPATH}" ]; then
+elif [ ! -f "${scriptpath}" ]; then
 	printf "Installing gitcrypty script\n"
-	cp "${SCRIPT}" "${SCRIPTPATH}"
-	if [ -f "${SCRIPTPATH}" ]; then
+	cp "${script}" "${scriptpath}"
+	if [ -f "${scriptpath}" ]; then
 		printf "Script successfully installed!\n"
 	else
 		printf "Error: installation failed\n"
